bcf4b35dab7135ea5e61f501e8594b467cbe7001
gregory <greg2502@gmail.com>
2014-01-29 20:13:06 -0800

    init the config

--- lib/sipwizard.rb
+++ lib/sipwizard.rb
@@ -2,4 +2,10 @@ Dir[File.dirname(__FILE__) + '/sipwizard/*.rb'].each{ |file| require file }
 
 module Sipwizard
   extend self
+
+  attr_accessor :config
+
+  def configure
+    @config = Configuration.new.tap{ |configuration| yield(configuration) }
+  end
 end
---
+++ spec/lib/sipwizard_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Sipwizard do
+  subject{ Sipwizard }
+
+  describe '.configure' do
+    let(:api_key){ 'johndoe_key' }
+
+    before do
+      subject.configure do |config|
+        config.api_key = api_key
+      end
+    end
+
+    it 'has set the api_key' do
+      subject.config.api_key.must_equal api_key
+    end
+  end
+end
