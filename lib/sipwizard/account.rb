3b9e0f6dcf7f9690060b3b69009117fd250faa0d
gregory <greg2502@gmail.com>
2014-02-08 15:42:54 -0800

    hack to get the filter to work

--- lib/sipwizard/account.rb
+++ lib/sipwizard/account.rb
@@ -87,13 +87,21 @@ module Sipwizard
       end
 
       def where(params)
-        @relation.where(params)
+        @relation.where( hash_to_query(params) )
         self
       end
 
       def count(nb)
         @relation.merge!({count: nb})
         self
+      end
+
+      private
+
+      #Hack to comply with the api spec ... which sucks
+      def hash_to_query(h)
+        h = Hash[h.map{|k,v| [k, "\"#{v}\""]}]
+        Rack::Utils.unescape Rack::Utils.build_query(h)
       end
     end
   end
