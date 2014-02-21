da3b9584ecc8e8bada44df709bd15bd476db8938
gregory <greg2502@gmail.com>
2014-02-20 19:34:47 -0800

    add get method + spec

--- spec/lib/sipwizard/dial_plan_spec.rb
+++ spec/lib/sipwizard/dial_plan_spec.rb
@@ -7,4 +7,15 @@ describe Sipwizard::DialPlan do
       expect(subject).to be_instance_of Fixnum
     end
   end
+
+  describe '.find(id)' do
+    let(:id){ settings['sensitive_data']['DIALPLAN_ID'] }
+
+    subject{ described_class.find(id) }
+
+    it 'returns an account' do
+      subject.should be_instance_of Sipwizard::DialPlan
+      subject.id.should eq id
+    end
+  end
 end
