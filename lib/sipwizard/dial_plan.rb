8347cd4aecd8c2fc8d84025d1dadbc7fa0507f17
gregory <greg2502@gmail.com>
2014-02-22 13:45:53 -0800

    add dialplan + specs

--- lib/sipwizard/dial_plan.rb
+++ lib/sipwizard/dial_plan.rb
@@ -2,19 +2,28 @@ module Sipwizard
   class DialPlan < Hashie::Trash
     API_PATH_MAP= {
       count: 'dialplan/count',
-      find: 'dialplan/get'
+      find: 'dialplan/get',
+      create: 'dialplan/add'
     }
 
     string_to_bool = ->(string) { string == "true" }
 
     property :id,                      from: :ID
-    property :dialplan_name,           from: :DialPlanName
+    property :dial_plan_name,          from: :DialPlanName
     property :trace_email_address,     from: :TraceEmailAddress
-    property :dialplan_script,         from: :DialPlanScript
+    property :dial_plan_script,        from: :DialPlanScript
     property :script_type_description, from: :ScriptTypeDescription
     property :accept_non_invite,       from: :AcceptNonInvite, transform_with: ->(b) { string_to_bool.call(b) }
 
     alias :accept_non_invite? :accept_non_invite
+
+    def self.build_for_request(h)
+      dial_plan = self.new(h)
+      dial_plan = Hash[dial_plan.map{ |k,v| ["#{k}".camelize, v] }]
+      dial_plan['ID'] = dial_plan.delete('Id')
+
+      dial_plan.delete_if{ |_,v| v.nil? } #delete all the keys for which we dont have value
+    end
 
     def self.count(params={})
       response = Connection.new.get(API_PATH_MAP[:count], params)
@@ -35,5 +44,13 @@ module Sipwizard
       self.new(result['Result'][0])
     end
 
+    def self.create(params)
+      payload = self.build_for_request(params)
+      result = Connection.new.post(API_PATH_MAP[:create], payload)
+
+      raise ArgumentError.new(result["Error"]) unless result['Success']
+
+      result['Result'] #ID
+    end
   end
 end
--- spec/lib/sipwizard/dial_plan_spec.rb
+++ spec/lib/sipwizard/dial_plan_spec.rb
@@ -18,4 +18,31 @@ describe Sipwizard::DialPlan do
       subject.id.should eq id
     end
   end
+
+  describe '.create(params)' do
+    let(:params) do
+      {
+        dial_plan_name: "foo",
+        dial_plan_script: "sys.Dial(\"music@iptel.org\"); sys.Respond(404, \"No one home\")"
+      }
+    end
+
+    subject{ described_class.create(params) }
+
+    it 'creates a new dial_plan' do
+      response = subject
+      expect(response).not_to be_nil
+      expect(response).to be_instance_of String
+      expect(response).to match(/(?:\w|-)+/)
+    end
+
+    context 'if the username already exists' do
+      #it 'raise an argument error' do
+        #expect do
+          #described_class.create(params)
+          #described_class.create({username: 'foo', password: 'bra'})
+        #end.to raise_exception(ArgumentError)
+      #end
+    end
+  end
 end
