01a4f26369b79ee8b673bbbfab50f8761c4126f7
gregory <greg2502@gmail.com>
2014-02-23 13:58:37 -0800

    add rate + spec

--- lib/sipwizard/rate.rb
+++ lib/sipwizard/rate.rb
@@ -1,13 +1,45 @@
 module Sipwizard
   class Rate < Hashie::Trash
     API_PATH_MAP={
-      count: 'rate/count'
+      count: 'rate/count',
+      create: 'rate/add',
+      find: 'rate/get'
     }
+
+    property :id,                from: :ID
+    property :description,       from: :Description
+    property :prefix,            from: :Prefix
+    property :rate,              from: :Rate
+    property :rate_code,         from: :RateCode
+    property :setup_cost,        from: :SetupCost
+    property :inserted,          from: :Inserted
+    property :increment_seconds, from: :IncrementSeconds
 
     def self.count(params={})
       response = connection.get(API_PATH_MAP[:count], params)
 
       response['Success'] ? response['Result'] : -1
+    end
+
+    def self.build_for_request(h)
+      rate = self.new(h)
+      rate = Hash[rate.map{ |k,v| ["#{k}".camelize, v] }]
+      rate['ID']                = rate.delete('Id')
+
+      rate.delete_if{ |_,v| v.nil? } #delete all the keys for which we dont have value
+    end
+
+    def self.create(params)
+      payload = self.build_for_request(params)
+      result = connection.post(API_PATH_MAP[:create], payload)
+
+      raise ArgumentError.new(result["Error"]) unless result['Success']
+
+      result['Result'] #ID
+    end
+
+    def self.where(params)
+      Relation.new.where(params)
     end
 
     private
--- spec/lib/sipwizard/rate_spec.rb
+++ spec/lib/sipwizard/rate_spec.rb
@@ -7,4 +7,34 @@ describe Sipwizard::Rate do
       expect(subject).to be_instance_of Fixnum
     end
   end
+
+  describe '.create(params)' do
+    let(:params) do
+      {
+        description: "foo",
+        prefix: "777",
+        rate: 2.0,
+        setup_cost: 0,
+        increment_seconds: 1
+      }
+    end
+
+    subject{ described_class.create(params) }
+
+    it 'creates a new rate' do
+      response = subject
+      expect(response).not_to be_nil
+      expect(response).to be_instance_of String
+      expect(response).to match(/(?:\w|-)+/)
+    end
+
+    context 'if the prefix already exists' do
+      it 'raise an argument error' do
+        expect do
+          described_class.create(params)
+          described_class.create({description: 'bar', prefix: "777", rate: 1.0, setup_cost: 0, increment_seconds: 1})
+        end.to raise_exception(ArgumentError)
+      end
+    end
+  end
 end
