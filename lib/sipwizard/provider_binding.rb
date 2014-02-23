6516df44bb7a9ad935c1a4068e2e647294c134f5
gregory <greg2502@gmail.com>
2014-02-23 15:38:34 -0800

    provider binding find

--- lib/sipwizard/provider_binding.rb
+++ lib/sipwizard/provider_binding.rb
@@ -1,13 +1,43 @@
 module Sipwizard
   class ProviderBinding < Hashie::Trash
     API_PATH_MAP={
-      count: 'sipproviderbinding/count'
+      count: 'sipproviderbinding/count',
+      find: 'sipproviderbinding/get'
     }
+
+    string_to_bool = ->(string) { string == "true" }
+
+    property :id,                          from: :ID
+    property :provider_id,                 from: :ProviderID
+    property :provider_name,               from: :ProviderName
+    property :registration_failur_message, from: :RegistrationFailureMessage
+    property :last_register_time,          from: :LastRegisterTime
+    property :next_registration_time,      from: :NextRegistrationTime
+    property :last_register_attempt,       from: :LastRegisterAttempt
+    property :is_registered,               from: :IsRegistered, transform_with: ->(b) { string_to_bool.call(b) }
+    property :binding_expiry,              from: :BindingExpiry
+    property :binding_uri,                 from: :BindingURI
+    property :registrar_sip_socket,        from: :RegistrarSIPSocket
+    property :cseq,                        from: :CSeq
 
     def self.count(params={})
       response = Connection.new.get(API_PATH_MAP[:count], params)
 
       response['Success'] ? response['Result'] : -1
+    end
+
+    def self.where(params)
+      Relation.new.where(params)
+    end
+
+    def self.find(id)
+      relation = self.where({ ID: id }).count(1)
+
+      result = Connection.new.get(API_PATH_MAP[:find], relation.relation)
+
+      return nil unless result['Success']
+
+      self.new(result['Result'][0])
     end
   end
 end
--- spec/lib/sipwizard/provider_binding_spec.rb
+++ spec/lib/sipwizard/provider_binding_spec.rb
@@ -8,4 +8,15 @@ describe Sipwizard::ProviderBinding do
       expect(subject).to be_instance_of Fixnum
     end
   end
+
+  describe '.find(id)' do
+    let(:id){ settings['sensitive_data']['PROVIDER_BINDING_ID'] }
+
+    subject{ described_class.find(id) }
+
+    it 'returns an provider binding' do
+      subject.should be_instance_of Sipwizard::ProviderBinding
+      subject.id.should eq id
+    end
+  end
 end
