63a684bdae3ca374cf5f3270d7f4a85cfa3bab1b
gregory <greg2502@gmail.com>
2014-02-22 17:16:32 -0800

    customer create

--- lib/sipwizard/customer.rb
+++ lib/sipwizard/customer.rb
@@ -1,14 +1,39 @@
 module Sipwizard
   class Customer < Hashie::Trash
     API_PATH_MAP={
-      count: 'customeraccount/count'
+      count: 'customeraccount/count',
+      create: 'customeraccount/add'
     }
 
+    property :id,             from: :ID
+    property :account_code,   from: :AccountCode
+    property :credit,         from: :Credit
+    property :account_name,   from: :AccountName
+    property :account_number, from: :AccountNumber
+    property :pin,            from: :PIN
 
     def self.count(params={})
       response = connection.get(API_PATH_MAP[:count], params)
 
       response['Success'] ? response['Result'] : -1
+    end
+
+    def self.build_for_request(h)
+      customer = self.new(h)
+      customer = Hash[customer.map{ |k,v| ["#{k}".camelize, v] }]
+      customer['ID']                = customer.delete('Id')
+      customer['PIN']               = customer.delete('Pin')
+
+      customer.delete_if{ |_,v| v.nil? } #delete all the keys for which we dont have value
+    end
+
+    def self.create(params)
+      payload = self.build_for_request(params)
+      result = connection.post(API_PATH_MAP[:create], payload)
+
+      raise ArgumentError.new(result["Error"]) unless result['Success']
+
+      result['Result'] #ID
     end
 
     private
--- spec/lib/sipwizard/customer_spec.rb
+++ spec/lib/sipwizard/customer_spec.rb
@@ -7,4 +7,31 @@ describe Sipwizard::Customer do
       expect(subject).to be_instance_of Fixnum
     end
   end
+
+  describe '.create(params)' do
+    let(:params) do
+      {
+        account_name: "foo",
+        credit: 4.0
+      }
+    end
+
+    subject{ described_class.create(params) }
+
+    it 'creates a new account' do
+      response = subject
+      expect(response).not_to be_nil
+      expect(response).to be_instance_of String
+      expect(response).to match(/(?:\w|-)+/)
+    end
+
+    context 'if the account_name already exists' do
+      it 'raise an argument error' do
+        expect do
+          described_class.create(params)
+          described_class.create({account_name: 'foo', credit: 3.0})
+        end.to raise_exception(ArgumentError)
+      end
+    end
+  end
 end
