93b0220c370b70e8c578620691d3baa76f86fa25
gregory <greg2502@gmail.com>
2014-04-05 10:55:43 -0700

    add specs

--- spec/lib/sipwizard/configuration_spec.rb
+++ spec/lib/sipwizard/configuration_spec.rb
@@ -2,12 +2,17 @@ require 'spec_helper'
 
 describe Sipwizard::Configuration do
   describe '.new' do
-    subject{ Sipwizard::Configuration.new(api_key) }
+    subject{ Sipwizard::Configuration.new(api_key, connection_params) }
 
     let(:api_key){ 'foo' }
+    let(:connection_params){ 'bar' }
 
     it 'has a api_key' do
       subject.api_key.should eq api_key
+    end
+
+    it 'has a connection_params' do
+      subject.connection_params.should eq connection_params
     end
   end
 end
